fa99c3a4a3b5a2dd19130620c13df507600b88d7
= <=>
2026-08-09 08:01:57 +0600

    User class and Tweet class are done

---
+++ TwitterClone/TwitterClone.Domain/Entities/Tweet.cs
@@ -0,0 +1,34 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class Tweet
+    {
+        private Guid _id;
+        private string _content;
+        private string _author;
+
+        public string Author { get; }
+        public string Content { get; private set; }
+
+
+        public Tweet(string author,string content) { 
+                
+            Author= author;
+            SetContent(content);
+        }
+
+        void SetContent(string content)
+        {
+            if (String.IsNullOrWhiteSpace(content))
+            {
+                throw new ArgumentNullException("Tweet cannot be empty.");
+            }
+
+            if (content.Length > 280)
+            {
+                throw new ArgumentException("Tweet cannot exceed 280 characters.");
+            }
+            _id = Guid.NewGuid();
+            Content= content;
+        }
+    }
+}
